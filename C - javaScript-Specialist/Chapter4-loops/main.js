f4784b58a933f1eef3915e0449fe57e397269bfa
sekrit <sneakypete411@gmail.com>
2017-09-26 13:37:17 -0400

    %*&#
    I haven't the words

--- C - javaScript-Specialist/Chapter4-loops/main.js
+++ C - javaScript-Specialist/Chapter4-loops/main.js
@@ -1,24 +1,23 @@
 window.onload = function () {
 
 document.getElementById("findNameBtn").addEventListener("click", lookForName);
+    
 };    
     function lookForName() {
         var myName = "Joe";
         var timesFound = [];
-        var text = document.getElementById("inputNameBox").value;
+        var text = document.getElementById("inputNameBox").value
+}
         
-        }
-        
-        for (var i = 0; i < (text.length; i++) {
+        for (var i = 0; i < text.length; i++) {
             if (text[i] === "J") {
-                for (var j = i; j < (myName.length + i); j++) {
-                    timesFound.push(text[j]);
-                    } if (timesFound.length === 0) {
-                    alert("Your name wasn't found!");
-                    console.log("should show that name wasn't found.");
-                        } else {
-                        alert(timesFound);
-                        console.log("Success, should show how many times it was found.");
-                    }
+            for (var j = i; j < (myName.length + i); j++) {
+                timesFound.push(text[j]);
+                }
             }
-        }        
+        } 
+                   if (timesFound.length === 0) {
+                    alert("Your name wasn't found!");
+                    } else {
+                        alert(timesFound);
+                        }
